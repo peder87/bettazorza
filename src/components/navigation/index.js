8474619602e3ae0ae20733d1e4d3a1dee64c8625
pietro grandi <pietrograndi@gmail.com>
2019-12-14 10:24:25 +0100

    remove unused

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Wrapper, NavigationWrapper } from "./style"
 import { MenuClosed, MenuOpen, LogoIcon } from "./components/icons"
 import Menu from "../menu"
-import Logo from "./components/logo.svg"
+// import Logo from "./components/logo.svg"
 
 export default ({ isOpen, toggleMenu }) => {
   return (
