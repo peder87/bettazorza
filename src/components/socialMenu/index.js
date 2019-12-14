8291e85a624f0b3933588b8da69149f0457c0360
pietro grandi <pietrograndi@gmail.com>
2019-12-14 10:09:26 +0100

    fix social socialmenu

--- src/components/socialMenu/index.js
+++ src/components/socialMenu/index.js
@@ -6,7 +6,6 @@ import { FacebookF } from "styled-icons/fa-brands/FacebookF"
 import { Behance } from "styled-icons/fa-brands/Behance"
 import { Dribbble } from "styled-icons/fa-brands/Dribbble"
 import { LinkedinIn } from "styled-icons/fa-brands/LinkedinIn"
-// import { opacify } from "polished"
 
 const MenuWrapper = styled.div`
   display: flex;
@@ -19,22 +18,25 @@ const MenuWrapper = styled.div`
 const IconWrapper = styled.div`
   display: flex;
   align-items: center;
-  width: 6.5em;
-  height: 2.5em;
+  width: 100%;
+  height: 3.6em;
   margin: 0 auto;
   border-right: ${props => (props.last ? "none" : `1px solid ${colors.white}`)};
-  &:hover {
-    color: red;
-  }
 `
 
-const icons = [Instagram, FacebookF, Behance, Dribbble, LinkedinIn]
+const icons = [
+  { icon: Instagram, url: "https://www.instagram.com/" },
+  { icon: FacebookF, url: "https://www.facebook.com/" },
+  { icon: Behance, url: "https://www.behance.net/" },
+  { icon: Dribbble, url: "https://dribbble.com/" },
+  { icon: LinkedinIn, url: "https://www.linkedin.com/feed/" },
+]
 
 const getIconStyled = icon => {
   return styled(icon)`
     color: ${colors.white};
-    width: 1.4em;
-    height: 1.4em;
+    width: 1.6em;
+    height: 1.6em;
     margin: 0 auto;
     opacity: 0.8;
     transition: opacity 0.25s linear;
@@ -46,14 +48,17 @@ const getIconStyled = icon => {
 }
 
 export const SocialMenu = () => {
+  const openLink = url => {
+    console.log(url)
+  }
+
   return (
     <MenuWrapper>
-      {icons.map((icon, index) => {
-        const StyledIcon = getIconStyled(icon)
+      {icons.map(({ icon, url }, index) => {
         const isLast = icons.length === index + 1
         const MyIcon = getIconStyled(icon)
         return (
-          <IconWrapper key={index} last={isLast}>
+          <IconWrapper key={index} last={isLast} onClick={() => openLink(url)}>
             <MyIcon />
           </IconWrapper>
         )
