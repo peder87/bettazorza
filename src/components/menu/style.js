0aa001e01f584d00e7ea23bcd3d78fc059ef0b46
Pietro Grandi <pietrograndi@gmail.com>
2020-05-25 23:43:07 +0200

    remove old menu

--- src/components/menu/style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-import { PageContainer } from "../pageComponents/style"
-
-export const MenuContainer = styled(PageContainer)`
-  position: relative;
-  height: 98vh;
-`
