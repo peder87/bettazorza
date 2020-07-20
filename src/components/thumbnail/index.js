bc88014b4556d475cab8818be9a99a356cbc52c1
Pietro Grandi <pietrograndi@gmail.com>
2020-07-20 08:53:51 +0200

    refactor thumbnail style syntax

--- src/components/thumbnail/index.js
+++ src/components/thumbnail/index.js
@@ -28,73 +28,63 @@ const ThumbWrapper = styled.div`
     margin: 0;
     padding: 0;
     overflow: hidden;
+
+    & img {
+      width: 100%;
+      max-width: 100%;
+      max-height: 100%;
+      display: block;
+    }
+
+    .background {
+      position: absolute;
+      top: 0;
+      width: 100%;
+      content: "";
+      background: ${colors.yellow};
+      min-height: 100%;
+      height: auto;
+      opacity: 0;
+      -webkit-transition: .3s ease-in-out;
+      transition: .3 ease-in-out;
+    }
+
+    .container {
+      position: absolute;
+      top: 40%;
+      width: 100%;
+
+      & span {
+        width: 80%; 
+        display: block;
+        position: relative;
+        top: 0;
+        height: auto;
+        margin: 0 auto;
+        padding: 0;
+        color: ${colors.purple};
+        font-weight: bold;
+        font-size: 24px;
+        text-transform: uppercase;
+        text-decoration: none;
+        text-align: center;
+        -webkit-transition: .3s ease-in-out;
+        transition: .3s ease-in-out;
+        opacity: 0;
+      }
+    }
+
+    &:hover {
+      cursor: pointer;
+      
+      & .background {
+        opacity: 0.9;
+      }
+
+      & .container span {
+        opacity: 1;
+      }
+    }
   }
 
-  & figure:hover {
-    cursor: pointer;
-  }
-
-  & figure div.background {
-    position: absolute;
-    top: 0;
-    width: 100%;
-    content: "";
-    background: ${colors.yellow};
-    min-height: 100%;
-    height: auto;
-    opacity: 0;
-    -webkit-transition: .3s ease-in-out;
-    transition: .3 ease-in-out;
-  }
-
-  & figure:hover div.background {
-    opacity: 0.90;
-  }
-
-
-  & figure img {
-    width: 100%;
-    max-width: 100%;
-    max-height: 100%;
-    display: block;
-    /* -webkit-filter: blur(0) saturate(1) brightness(1);
-    filter: blur(0) saturate(1) brightness(1); */
-    -webkit-transition: .3s ease-in-out;
-    transition: .3 ease-in-out;
-  }
-
-  & figure:hover img {
-    /* filter: blur(3px) saturate(.45) brightness(.85);
-    -webkit-filter: blur(3px) saturate(.45) brightness(.85); */
-  }
-
-  & figure div.container {
-    position: absolute;
-    top: 40%;
-    width: 100%;
-
-  }
-
-  & figure div.container span {
-    width: 80%; 
-    display: block;
-    position: relative;
-    top: 0;
-    height: auto;
-    margin: 0 auto;
-    padding: 0;
-    color: ${colors.purple};
-    font-weight: bold;
-    font-size: 24px;
-    text-transform: uppercase;
-    text-decoration: none;
-    text-align: center;
-    -webkit-transition: .3s ease-in-out;
-    transition: .3s ease-in-out;
-    opacity: 0;
-  }
-
-  & figure:hover div.container span {
-    opacity: 1;
-  }
 `
