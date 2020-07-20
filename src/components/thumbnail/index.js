2b238876f0116b3bfbb62a0fa663ec964c98852a
Pietro Grandi <pietrograndi@gmail.com>
2020-07-20 08:45:17 +0200

    fix thumbnail backdrop

--- src/components/thumbnail/index.js
+++ src/components/thumbnail/index.js
@@ -11,7 +11,8 @@ export const Thumbnail = (props) =>
         alt={props.title}
         src={props.img}
       />
-      <div>
+      <div className="background" />
+      <div className="container">
         <span>{props.title}</span>
       </div>
     </figure>
@@ -33,32 +34,56 @@ const ThumbWrapper = styled.div`
     cursor: pointer;
   }
 
+  & figure div.background {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    content: "";
+    background: ${colors.yellow};
+    min-height: 100%;
+    height: auto;
+    opacity: 0;
+    -webkit-transition: .3s ease-in-out;
+    transition: .3 ease-in-out;
+  }
+
+  & figure:hover div.background {
+    opacity: 0.90;
+  }
+
+
   & figure img {
     width: 100%;
     max-width: 100%;
     max-height: 100%;
     display: block;
-    -webkit-filter: blur(0) saturate(1) brightness(1);
-    filter: blur(0) saturate(1) brightness(1);
+    /* -webkit-filter: blur(0) saturate(1) brightness(1);
+    filter: blur(0) saturate(1) brightness(1); */
     -webkit-transition: .3s ease-in-out;
     transition: .3 ease-in-out;
   }
 
   & figure:hover img {
-    filter: blur(3px) saturate(.45) brightness(.85);
-    -webkit-filter: blur(3px) saturate(.45) brightness(.85);
+    /* filter: blur(3px) saturate(.45) brightness(.85);
+    -webkit-filter: blur(3px) saturate(.45) brightness(.85); */
   }
 
-  & figure span {
+  & figure div.container {
     position: absolute;
     top: 40%;
-    left: 0;
-    display: block;
     width: 100%;
+
+  }
+
+  & figure div.container span {
+    width: 80%; 
+    display: block;
+    position: relative;
+    top: 0;
     height: auto;
-    margin: 0;
+    margin: 0 auto;
     padding: 0;
-    color: ${colors.yellow};
+    color: ${colors.purple};
     font-weight: bold;
     font-size: 24px;
     text-transform: uppercase;
@@ -69,7 +94,7 @@ const ThumbWrapper = styled.div`
     opacity: 0;
   }
 
-  & figure:hover span {
+  & figure:hover div.container span {
     opacity: 1;
   }
 `
