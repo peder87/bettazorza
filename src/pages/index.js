c43ffb361e661d9524e319fba87f4c026c9e84b8
Pietro Grandi <pietrograndi@gmail.com>
2020-07-28 16:43:00 +0200

    added footer in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,24 @@ import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 import Home from './home'
+import { Footer } from '../components/footer'
 
 export default (props) => {
   console.log('index props', props)
+  const style = { 
+    bgcolor: 'purple',
+    color: 'yellow'
+  }
   return (
-    <StaticQuery 
-      query={query}
-      render={({allProjectsJson}) => {
-        const style = { 
-          bgcolor: 'purple',
-          color: 'yellow'
-        }
-        return (<Layout pageConf={style}>
-          <Home data={allProjectsJson}/>
-        </Layout>)
-      }}
-    />
+    <Layout pageConf={style}>
+      <StaticQuery 
+        query={query}
+        render={({allProjectsJson}) => {
+          return (<Home data={allProjectsJson}/> )
+        }}
+      />
+      <Footer />
+    </Layout>
   )
 }
 
