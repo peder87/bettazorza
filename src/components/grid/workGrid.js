5180d44483d2a90993bee6b21b50044f49315092
Pietro Grandi <pietrograndi@gmail.com>
2020-11-23 23:24:44 +0100

    progress workTemplate

--- src/components/grid/workGrid.js
+++ src/components/grid/workGrid.js
@@ -4,6 +4,7 @@ import { PageTitle, CopyWrapper, PageCopy, BzTitleProject } from "../typography"
 import { Image } from "../image/image"
 import { Video } from "../video"
 import { SectionCenteredContent } from "../pageComponents/style"
+import { HeroHeader } from "../hero/style"
 import SEO from "../../components/seo"
 
 export const WorkGrid = ({ data }) => {
@@ -12,35 +13,12 @@ export const WorkGrid = ({ data }) => {
     content: data.tags.join(", "),
   }
   return (
-    <SectionCenteredContent>
-      <SEO title={data.title} description={data.caption} meta={[seoCat]} />
-      <BzTitleProject bold>{data.title}</BzTitleProject>
-      <BzTitleProject noPadding>{data.tags[0]}</BzTitleProject>
-      {/* <CopyWrapper last>
-        <PageCopy>{data.caption}</PageCopy>
-      </CopyWrapper> */}
-      {/* <CopyWrapper last>
-          {data.tags.length > 0 &&
-            data.tags.map((tag, i) => {
-              const isLast = i === data.tags.length - 1
-              return !isLast ? (
-                <span className="tag" key={tag}>
-                  {tag} -
-                </span>
-              ) : (
-                <span className="tag" key={tag}>
-                  {tag}
-                </span>
-              )
-            })}
-        </CopyWrapper> */}
-      <ProjectGrid>
-        {data.imgs.map(img => (
-          <Item key={img.alt} full={img.fullWidth}>
-            {img.mediaType === "img" ? <Image {...img} /> : <Video {...img} />}
-          </Item>
-        ))}
-      </ProjectGrid>
-    </SectionCenteredContent>
+    <ProjectGrid>
+      {data.imgs.map(img => (
+        <Item key={img.alt} full={img.fullWidth}>
+          {img.mediaType === "img" ? <Image {...img} /> : <Video {...img} />}
+        </Item>
+      ))}
+    </ProjectGrid>
   )
 }
--- src/templates/work-template.js
+++ src/templates/work-template.js
@@ -1,13 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
 import { WorkGrid } from "../components/grid/workGrid"
+import { SectionCenteredContent } from "../components/pageComponents/style"
+import { LogoHeader, WorkTitle } from "./style"
 import { Bottom } from "../components/bottom"
+import { navigate } from "gatsby"
+
+const handleClick = () => {
+  navigate("/")
+}
 
 export default ({ pageContext, ...props }) => {
   const { navigationJson, projectsJson } = pageContext.pageData.data
   return (
     <Layout pageConf={navigationJson}>
-      <WorkGrid data={projectsJson} />
+      <SectionCenteredContent>
+        <LogoHeader>
+          <img onClick={handleClick} src="/images/common/logo_betta.svg" />
+        </LogoHeader>
+        <WorkTitle bold>{projectsJson.title}</WorkTitle>
+        <WorkTitle>{projectsJson.tags[0]}</WorkTitle>
+        <WorkGrid data={projectsJson} />
+      </SectionCenteredContent>
       <Bottom />
     </Layout>
   )
