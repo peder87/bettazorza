205f63a834ce74c6bfc662f360537265a51e3e9a
Pietro Grandi <pietrograndi@gmail.com>
2020-11-29 16:12:00 +0100

    progress index

--- src/pages/stack/index.js
+++ src/pages/stack/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import idx from "idx"
+import has from "has"
 import { Hero } from "../../components/hero"
 import { About } from "../../components/about"
 import { Portfolio } from "../../components/portfolio"
@@ -7,18 +9,28 @@ import { Bottom } from "../../components/bottom"
 import { cleanOrder, getDictionaryById } from "../../utils/dataUtils"
 
 export const Stack = ({ about, order, projects, works }) => {
-  const list = cleanOrder(order.edges)
-  const mapProjects = getDictionaryById(projects.edges, "id")
+  const ord = has(order, "edges") ? order.edges : []
+  const proj = has(projects, "edges") ? projects.edges : []
+  const worksList = has(works, "nodes") ? works.nodes : []
+
+  const list = cleanOrder(ord)
+  const mapProjects = getDictionaryById(proj, "id")
 
   return (
     <div>
       <Hero />
       <About data={about} />
       <Portfolio list={list} src={mapProjects} />
-      <ServiceList list={works.nodes} />
+      <ServiceList list={worksList} />
       <Bottom />
     </div>
   )
+}
+
+Stack.defaultProps = {
+  order: {},
+  projects: {},
+  works: {},
 }
 
 export default Stack
