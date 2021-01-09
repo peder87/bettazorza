a5cca3d30a8856331fb96e418ccac5970e908e72
Pietro Grandi <pietrograndi@gmail.com>
2021-01-09 10:27:01 +0100

    testing analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-3S8WMR11MJ",
+        head: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         googleAnalytics: {
