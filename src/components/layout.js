b38f752f5d232efc30f7dae14884c52b23da098a
Pietro Grandi <pietrograndi@gmail.com>
2021-01-09 16:39:14 +0100

    cookie banner msg

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,8 +19,8 @@ const Layout = ({ children }) => {
         overlay={true}
         expires={150}
       >
-        Questo sito o gli strumenti terzi da questo utilizzati si avvalgono di
-        cookie necessari al funzionamento
+        Questo sito o gli strumenti terzi avvalgono di cookie necessari al
+        funzionamento per garantire un' esperienza utente ottimale.
       </CookieConsent>
     </>
   )
