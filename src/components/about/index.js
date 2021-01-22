6520ea2933617aea2eaed175f74d1c7ae710c970
Pietro Grandi <pietrograndi@gmail.com>
2021-01-22 11:55:45 +0100

    fix wrong email

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -44,7 +44,7 @@ export const About = ({ img }) => {
         </BzAboutGrid>
         <AboutFooter>
           <BzButton
-            text="hello@bettazorza.com"
+            text="bettazorza@gmail.com"
             first={colors.cyan}
             click={click}
           />
